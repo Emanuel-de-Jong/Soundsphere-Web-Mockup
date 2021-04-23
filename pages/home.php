b1a969f74832d2ab7a17e55e72227f33372694c0
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-04-23 15:52:32 +0200

    Home video base

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,18 @@
+<div style="height: 400px;">
+    <video playsinline autoplay muted loop poster="bg.mp4" id="bgvid">
+        <source src="../resources/bg.mp4" type="video/mp4">
+    </video>
+    <div class="container" style="height: 100%;">
+        <div class="row align-items-center" style="height: 100%;">
+            <div class="text-center">
+                <span class="headline">
+                    soundsphere
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
 <main role="main">
     <div class="container">
 
