d4a791bc6336da305d92447ca624688d13a1e0e4
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-05-08 16:42:45 +0200

    Subnavbar styling

--- components/subnavbar.php
+++ components/subnavbar.php
@@ -1,7 +1,15 @@
-<div id="secondary-navbar" class="nav-scroller bg-body shadow-sm">
-    <nav class="nav nav-underline">
-        <a class="nav-link" href="#">Summary</a>
-        <a class="nav-link" href="#">Top</a>
-        <a class="nav-link" href="#">Recent</a>
-    </nav>
-</div>
+<nav id="secondary-navbar" class="fixed-top shadow-sm">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="">Summary</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">Top</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">Recent</a>
+        </li>
+    </ul>
+</nav>
+
+<div style="min-height: 40px;"></div>
