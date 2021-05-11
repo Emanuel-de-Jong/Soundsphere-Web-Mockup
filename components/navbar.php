429280d3e25752b3bb71d8dc8771793882ca454b
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-05-11 16:22:20 +0200

    Remove navbar searchbar

--- components/navbar.php
+++ components/navbar.php
@@ -3,11 +3,13 @@
         <a class="navbar-brand ms-4 me-4" href="/">
             <img src="resources/logo-trans.png" width="40" />
         </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mb-2 mb-md-0 me-2">
+            <ul class="navbar-nav mb-2 mb-md-0 me-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="/">Home</a>
                 </li>
@@ -33,12 +35,7 @@
                     <a class="nav-link" href="https://github.com/semyon422/soundsphere-updater/archive/master.zip" target="_blank">Download</a>
                 </li>
             </ul>
-            <form class="me-auto" action="?a=search" method="post">
-                <div class="input-group">
-                    <input class="form-control" type="search" placeholder="Search..." aria-label="Search" />
-                    <button class="btn btn-outline-light" type="submit"><i class="fas fa-search"></i></button>
-                </div>
-            </form>
+
             <ul class="navbar-nav mb-2 mb-md-0 me-4">
                 <?php
                     if(isset($_COOKIE["logged-in"])) {
--- pages/leaderboards/leaderboard1.php
+++ pages/leaderboards/leaderboard1.php
@@ -3,19 +3,32 @@
         <div class="main-box">
             <h1>Leaderboard1</h1>
             
-            <p>
-                Maecenas at orci feugiat, pharetra est quis, vulputate neque. Vestibulum elementum, orci nec laoreet pretium, lectus
-                velit viverra lacus, at blandit lorem purus sed ex. Cras erat elit, condimentum at dignissim ut, sodales sit amet
-                lorem. Nullam sagittis felis enim, et congue mi facilisis a. Ut maximus hendrerit lectus. Aliquam sit amet lacus quis
-                mi semper rhoncus. Quisque a orci tincidunt, posuere risus eget, vehicula purus. Mauris id leo a massa vulputate
-                venenatis at ac nisl. Nullam non aliquet augue. Donec eu urna at magna feugiat tristique. Nunc non dui ut nisl
-                faucibus rutrum. Aenean vulputate interdum vulputate. Curabitur auctor, dolor sit amet viverra laoreet, elit metus
-                dictum massa, ut dapibus purus nibh ac eros. Duis eget finibus purus, vel efficitur mauris. Etiam id risus vitae nunc
-                lobortis luctus non at mauris. Phasellus ut lorem massa. In non eleifend erat. Nam dapibus mauris et eros pretium, et
-                malesuada ante varius. Quisque ac odio vel mauris pretium suscipit sit amet in quam. Phasellus id erat consectetur,
-                sollicitudin velit quis, suscipit dui. Morbi sit amet faucibus felis.
-                enim.
-            </p>
+            <table class="data-table table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>Test</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </main>
 </div>
