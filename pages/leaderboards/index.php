c508ae4c2ece1f9186b66979a6ed4bfbe31503e3
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-05-26 18:13:42 +0200

    Leaderboards join/copy select community modal

--- pages/leaderboards/index.php
+++ pages/leaderboards/index.php
@@ -3,8 +3,29 @@
         <div class="main-box">
             <h1>Leaderboards</h1>
 
-            
-            <?php require('components/confirm-modal.php'); ?>
+
+            <div class="modal" id="add-leaderboard-modal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Add leaderboard</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div class="modal-body">
+                            <select class="form-select">
+                                <option selected>Select a community</option>
+                                <option value="Leaderboard1">Community1</option>
+                                <option value="Leaderboard2">Community2</option>
+                                <option value="Leaderboard3">Community3</option>
+                            </select>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn bg-dgreen bg-dgreen-h" data-bs-dismiss="modal">Add</button>
+                            <button type="button" class="btn bg-dgray bg-dgray-h" data-bs-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <section>
                 <table class="data-table data-table-all table">
                     <thead>
@@ -23,10 +44,10 @@
                             <td><a href="?a=players_player1_index">Player1</a></td>
                             <td>10</td>
                             <td class="table-icon-row">
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-sign-in-alt"></i>
                                 </a>
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-copy"></i>
                                 </a>
                             </td>
@@ -37,10 +58,10 @@
                             <td><a href="?a=players_player1_index">Player1</a></td>
                             <td>10</td>
                             <td class="table-icon-row">
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-sign-in-alt"></i>
                                 </a>
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-copy"></i>
                                 </a>
                             </td>
@@ -51,10 +72,10 @@
                             <td><a href="?a=players_player1_index">Player1</a></td>
                             <td>10</td>
                             <td class="table-icon-row">
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-sign-in-alt"></i>
                                 </a>
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-copy"></i>
                                 </a>
                             </td>
@@ -65,10 +86,10 @@
                             <td><a href="?a=players_player1_index">Player1</a></td>
                             <td>10</td>
                             <td class="table-icon-row">
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-sign-in-alt"></i>
                                 </a>
-                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#confirm-modal">
+                                <a href="#" class="table-icon blue blue-h" data-bs-toggle="modal" data-bs-target="#add-leaderboard-modal">
                                     <i class="fas fa-copy"></i>
                                 </a>
                             </td>
