56472dfe2486412098ad37de1477893418eb098c
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-07-13 15:04:15 +0200

    WIP image upload

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,4 +7,34 @@ $(function () {
         scrollX: true,
         order: [[ 2, "desc" ]]
     });
+
+    $(".form-settings input[type=file]").on("dragenter", function() {
+        var input = $(this);
+        input.addClass("input-file-dragenter");
+    });
+
+    $(".form-settings input[type=file]").on("dragleave drop", function() {
+        var input = $(this);
+        input.removeClass("input-file-dragenter");
+        input.focus();
+    });
+
+    $("input.input-image").on("drop", function () {
+        var input = $(this);
+        var filename = input.val();
+    
+        var acceptedExtensions = ['png', 'jpg', 'jpeg'];
+        if ($.inArray(filename.split('.').pop().toLowerCase(), acceptedExtensions) == -1)
+        {
+            console.log("wrong");
+            $(this).val("");
+            console.log("val: " + $(this).val());
+        } else {
+            console.log("right");
+        }
+    });
+
+    $("input.input-image").hover(function () {
+        console.log("val: " + $(this).val() + "\ntype: " + typeof $(this).val());
+    });
 });
--- src/pages/players/player1/settings.php
+++ src/pages/players/player1/settings.php
@@ -12,12 +12,17 @@
                 <form class="form-settings" action="?a=players_player1_index" method="post">
                     <div class="form-item">
                         <label for="name" class="form-label" title="Your username">Name</label>
-                        <input type="text" class="form-control" name="name" value="Player1">
+                        <input type="text" class="form-control" id="name" name="name" value="Player1">
                     </div>
 
                     <div class="form-item">
                         <label for="description" class="form-label" title="Page description">Description</label>
-                        <textarea class="form-control" name="description">Maecenas at orci feugiat, pharetra est quis, vulputate neque. Vestibulum elementum, orci nec laoreet pretium, lectus velit viverra lacus, at blandit lorem purus sed ex. Cras erat elit, condimentum at dignissim ut, sodales sit amet lorem. Nullam sagittis felis enim, et congue mi facilisis a. Ut maximus hendrerit lectus. Aliquam sit amet lacus quis mi semper rhoncus. Quisque a orci tincidunt, posuere risus eget, vehicula purus. Mauris id leo a massa vulputate venenatis at ac nisl. Nullam non aliquet augue. Donec eu urna at magna feugiat tristique. Nunc non dui ut nisl faucibus rutrum. Aenean vulputate interdum vulputate. Curabitur auctor, dolor sit amet viverra laoreet, elit metus dictum massa, ut dapibus purus nibh ac eros. Duis eget finibus purus, vel efficitur mauris. Etiam id risus vitae nunc lobortis luctus non at mauris. Phasellus ut lorem massa. In non eleifend erat. Nam dapibus mauris et eros pretium, et malesuada ante varius. Quisque ac odio vel mauris pretium suscipit sit amet in quam. Phasellus id erat consectetur, sollicitudin velit quis, suscipit dui. Morbi sit amet faucibus felis. enim.</textarea>
+                        <textarea class="form-control" id="description" name="description">Maecenas at orci feugiat, pharetra est quis, vulputate neque. Vestibulum elementum, orci nec laoreet pretium, lectus velit viverra lacus, at blandit lorem purus sed ex. Cras erat elit, condimentum at dignissim ut, sodales sit amet lorem. Nullam sagittis felis enim, et congue mi facilisis a. Ut maximus hendrerit lectus. Aliquam sit amet lacus quis mi semper rhoncus. Quisque a orci tincidunt, posuere risus eget, vehicula purus. Mauris id leo a massa vulputate venenatis at ac nisl. Nullam non aliquet augue. Donec eu urna at magna feugiat tristique. Nunc non dui ut nisl faucibus rutrum. Aenean vulputate interdum vulputate. Curabitur auctor, dolor sit amet viverra laoreet, elit metus dictum massa, ut dapibus purus nibh ac eros. Duis eget finibus purus, vel efficitur mauris. Etiam id risus vitae nunc lobortis luctus non at mauris. Phasellus ut lorem massa. In non eleifend erat. Nam dapibus mauris et eros pretium, et malesuada ante varius. Quisque ac odio vel mauris pretium suscipit sit amet in quam. Phasellus id erat consectetur, sollicitudin velit quis, suscipit dui. Morbi sit amet faucibus felis. enim.</textarea>
+                    </div>
+
+                    <div class="form-item">
+                        <label for="banner" class="form-label" title="A blurred version of an image. Shown behind your profile page.">Profile banner</label>
+                        <input type="file" class="form-control input-image" name="banner" id="banner" accept=".png, .jpg, .jpeg">
                     </div>
 
                     <?php require('components/modals/add-input-mode.php'); ?>
