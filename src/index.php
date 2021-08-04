06c476b381d67d68db36db9db2899cd98d4ed304
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-08-04 20:57:16 +0200

    Update jQuery from 3.3.1 to 3.5.1

--- src/index.php
+++ src/index.php
@@ -39,7 +39,7 @@
 
     <?php require('components/footer.php'); ?>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
