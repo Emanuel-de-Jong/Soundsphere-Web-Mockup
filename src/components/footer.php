d44d79ff58b911e79c73f6ebd071e9a53f7fa772
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-08-04 15:44:34 +0200

    Improve footer social icons

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,13 +1,13 @@
 <footer class="footer text-center mt-auto">
     <div class="container">
         <div class="social">
-            <a href="https://discord.gg/ubKMtTk">
+            <a href="https://discord.gg/ubKMtTk" class="social-icon">
                 <i class="fab fa-discord"></i>
             </a>
-            <a href="https://github.com/semyon422/soundsphere">
+            <a href="https://github.com/semyon422/soundsphere" class="social-icon social-icon-l">
                 <i class="fab fa-github"></i>
             </a>
-            <a href="https://www.patreon.com/semyon422">
+            <a href="https://www.patreon.com/semyon422" class="social-icon">
                 <i class="fab fa-patreon"></i>
             </a>
         </div>
