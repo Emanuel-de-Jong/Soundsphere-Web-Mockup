5b3bb3e929e52ee9e64c0f8135bf163ce4e9b12b
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-08-04 21:01:35 +0200

    Restructure html template

--- src/index.php
+++ src/index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en" class="h-100">
-
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
-    <meta name="generator" content="Hugo 0.82.0" />
-
-    <title>soundsphere</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" />
@@ -16,6 +10,8 @@
     <link rel="stylesheet" href="css/styles.css" />
     
     <script src="https://kit.fontawesome.com/dbaf22a3a8.js"></script>
+
+    <title>soundsphere</title>
 </head>
 
 <body class="d-flex flex-column">
@@ -45,5 +41,4 @@
     <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
     <script src="js/custom.js"></script>
 </body>
-
 </html>
