350001129525c101efe8c2052cf8fc97da6d162b
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-08-12 01:02:19 +0200

    I accidentally replaced all jQuery but e

--- src/js/custom-plain.js
+++ src/js/custom-plain.js
@@ -10,13 +10,13 @@ ready(() => {
 
 
         document.querySelectorAll(".data-table-all").forEach(table => {
-            dtInit($(table), {
+            dtInit(table, {
                 ...dtDefaultOptions,
             });
         });
 
         document.querySelectorAll(".data-table-slim").forEach(table => {
-            dtInit($(table), {
+            dtInit(table, {
                 ...dtDefaultOptions,
                 columnDefs: [
                     { orderable: false, targets: ["_all"] }
@@ -28,11 +28,11 @@ ready(() => {
         });
 
 
-        function dtInit(jTable, options) {
-            options["order"] = dtOrder(jTable);
+        function dtInit(table, options) {
+            options["order"] = dtOrder(table);
 
-            const insertTopId = jTable.attr("data-insert-top");
-            const insertBottomId = jTable.attr("data-insert-bottom");
+            const insertTopId = table.getAttribute("data-insert-top");
+            const insertBottomId = table.getAttribute("data-insert-bottom");
 
             let insertTopHTML;
             let domTop = "<'row'<'col'l><'col'f>>";
@@ -55,7 +55,7 @@ ready(() => {
                     domBottom;
             }
 
-            jTable.DataTable({
+            $(table).DataTable({
                 ...options,
                 initComplete: function(settings, json) {
                     if (insertTopId != null)
@@ -68,11 +68,11 @@ ready(() => {
         }
 
 
-        function dtOrder(jTable) {
+        function dtOrder(table) {
             let orderItems = [];
             for (let i=0; i<10; i++) {
-                let col = jTable.attr("data-c" + i);
-                let dir = jTable.attr("data-d" + i);
+                let col = table.getAttribute("data-c" + i);
+                let dir = table.getAttribute("data-d" + i);
 
                 if (col == null)
                     break;
@@ -91,16 +91,17 @@ ready(() => {
 
 
         function dtInsertGet(insertId) {
-            let jInsert = $("#" + insertId);
-            const insertHTML = jInsert.html();
+            let insert = document.getElementById(insertId);
 
-            jInsert.remove();
+            const insertHTML = insert.innerHTML;
+
+            insert.parentNode.removeChild(insert);
 
             return insertHTML;
         }
 
         function dtInsertSet(insertId, insertHTML) {
-            $("div." + insertId).replaceWith(insertHTML);
+            document.querySelector("." + insertId).outerHTML = insertHTML;
         }
     }
 
@@ -108,12 +109,10 @@ ready(() => {
 
     
     if (document.querySelector("form") != null) {
-        document.querySelectorAll("form").forEach(form => { $(form).attr("novalidate", true); });
+        document.querySelectorAll("form").forEach(form => form.setAttribute("novalidate", true));
 
         querySelectorAlls(["input", "select", "textarea"]).forEach(input => {
-            let jInput = $(input);
-            
-            let invalidMessage = jInput.attr("data-invalid");
+            let invalidMessage = input.getAttribute("data-invalid");
 
             if (invalidMessage != null) {
                 input.setCustomValidity(invalidMessage);
@@ -121,8 +120,8 @@ ready(() => {
                 invalidMessage = "";
             }
 
-            if (jInput.siblings(".form-invalid-message")[0] == null) {
-                jInput.after("<div class=\"form-invalid-message\">" + invalidMessage + "</div>");
+            if (getSiblingByClass(input, "form-invalid-message") == null) {
+                input.insertAdjacentHTML("afterend", "<div class=\"form-invalid-message\">" + invalidMessage + "</div>");
             }
         });
 
@@ -133,17 +132,16 @@ ready(() => {
 
 
         addEventListeners(querySelectorAlls(["input", "select", "textarea"]), "invalid", (e) => {
-            showInvalidMessage($(e.target));
+            showInvalidMessage(e.target);
         });
 
 
         addEventListeners(querySelectorAlls(["input", "select", "textarea"]), "change", (e) => {
             let input = e.target;
-            let jInput = $(input);
 
             if (input.validity.customError) {
                 input.setCustomValidity("");
-                hideInvalidMessage(jInput);
+                hideInvalidMessage(input);
             }
         });
 
@@ -158,28 +156,26 @@ ready(() => {
 
 
         function validateInput(input) {
-            let jInput = $(input);
-            
             if (input.validity.valid) {
-                hideInvalidMessage(jInput);
+                hideInvalidMessage(input);
             } else {
-                showInvalidMessage(jInput);
+                showInvalidMessage(input);
             }
         }
 
         
-        function showInvalidMessage(jInput) {
-            jInput.addClass("form-invalid");
+        function showInvalidMessage(input) {
+            input.classList.add("form-invalid");
 
-            let jMessageDiv = jInput.siblings(".form-invalid-message");
-            jMessageDiv.html(jInput.get(0).validationMessage);
+            let messageDiv = getSiblingByClass(input, "form-invalid-message");
+            messageDiv.innerHTML = input.validationMessage;
         }
 
-        function hideInvalidMessage(jInput) {
-            jInput.removeClass("form-invalid");
+        function hideInvalidMessage(input) {
+            input.classList.remove("form-invalid");
 
-            let jMessageDiv = jInput.siblings(".form-invalid-message");
-            jMessageDiv.html("");
+            let messageDiv = getSiblingByClass(input, "form-invalid-message");
+            messageDiv.innerHTML = "";
         }
     }
     
@@ -188,24 +184,27 @@ ready(() => {
 
     if (document.querySelector("input[type=file]") != null) {
         addEventListeners(document.querySelectorAll("input[type=file]"), "dragenter", (e) => {
-            let jInput = $(e.target);
-            jInput.addClass("input-file-dragenter");
+            let input = e.target;
+
+            input.classList.add("input-file-dragenter");
         });
 
         addEventsListeners(document.querySelectorAll("input[type=file]"), ["dragleave", "drop"], (e) => {
-            let jInput = $(e.target);
-            jInput.removeClass("input-file-dragenter");
-            jInput.focus();
+            let input = e.target;
+
+            input.classList.remove("input-file-dragenter");
+            input.focus();
         });
 
         const acceptedExtensions = ["png", "jpg", "jpeg"];
         addEventListeners(document.querySelectorAll("input.input-image"), "change", (e) => {
-            let jInput = $(e.target);
-            const filepath = jInput.val();
-            if ($.inArray(filepath.split(".").pop().toLowerCase(), acceptedExtensions) == -1)
+            let input = e.target;
+
+            const filepath = input.value;
+            if (acceptedExtensions.indexOf(filepath.split(".").pop().toLowerCase()) == -1)
             {
                 e.preventDefault();
-                $(this).val("");
+                input.value = "";
             }
         });
     }
@@ -219,9 +218,9 @@ ready(() => {
         document.getElementById("username").addEventListener("change", showPlayerName);
 
         function showPlayerName() {
-            const colorL = $("#player-edit #username-color-l").val();
-            const colorR = $("#player-edit #username-color-r").val();
-            const name = $("#player-edit #username").val();
+            const colorL = document.getElementById("username-color-l").value;
+            const colorR = document.getElementById("username-color-r").value;
+            const name = document.getElementById("username").value;
 
             let preview = document.getElementById("username-preview");
             preview.innerHTML = name;
--- src/js/helper.js
+++ src/js/helper.js
@@ -27,3 +27,14 @@ function querySelectorAlls(selectors) {
     selectors.forEach(selector => elementArrays.push(Array.from(document.querySelectorAll(selector))));
     return elementArrays.flat();
 }
+
+function getSiblingByClass(element, className) {
+    let sibling = element.parentNode.firstElementChild;
+
+    while (sibling) {
+        if (sibling.classList.contains(className))
+            return sibling;
+        
+        sibling = sibling.nextElementSibling
+    }
+}
