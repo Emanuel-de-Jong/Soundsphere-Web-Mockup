7ccec20e289e500e848b26c617bf9749cfb17496
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-08-11 19:45:02 +0200

    Move color classes to styles.scss to fix duplicate code

--- src/index.php
+++ src/index.php
@@ -6,7 +6,6 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" />
-    <link rel="stylesheet" href="css/colors.css" />
     <link rel="stylesheet" href="css/styles.css" />
     
     <script src="https://kit.fontawesome.com/dbaf22a3a8.js"></script>
